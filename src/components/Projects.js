ab45efcfdca195c378e6f1c3964cff014e1c1663
Sahil A <sahil.arshad.a@gmail.com>
2025-06-04 19:27:39 +0530

    Updates

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -63,7 +63,7 @@ export default function Projects() {
     const handleTouchMove = (e) => {
       if (!isDragging.current) return;
       const x = e.touches[0].clientX;
-      const walk = (startX.current - x) * 5;
+      const walk = (startX.current - x) * 10;
       container.scrollLeft = scrollLeft.current + walk;
     };
 
